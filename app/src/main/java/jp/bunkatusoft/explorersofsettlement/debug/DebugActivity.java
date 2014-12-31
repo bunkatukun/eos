4293c8d6856df89259b5596453e031cfb7b92b46
erunga <aerialis3@gmail.com>
2014-12-31 21:18:47 +0900

    [clean]インデントを整えた

--- app/src/main/java/jp/bunkatusoft/explorersofsettlement/debug/DebugActivity.java
+++ app/src/main/java/jp/bunkatusoft/explorersofsettlement/debug/DebugActivity.java
@@ -16,32 +16,32 @@ import jp.bunkatusoft.explorersofsettlement.field.SettlementFieldActivity;
  */
 public class DebugActivity extends FragmentActivity implements View.OnClickListener {
 
-	Context mContext;
-	LinearLayout mBaseLayout;
+    Context mContext;
+    LinearLayout mBaseLayout;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		mContext = this;
-		mBaseLayout = new LinearLayout(mContext);
-		mBaseLayout.setOrientation(LinearLayout.VERTICAL);
-		setContentView(mBaseLayout);
+        mContext = this;
+        mBaseLayout = new LinearLayout(mContext);
+        mBaseLayout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(mBaseLayout);
 
         mBaseLayout.addView(createButtonStartSettlement());
         mBaseLayout.addView(createButtonStartField());
-	}
+    }
 
-    private Button createButtonStartSettlement(){
+    private Button createButtonStartSettlement() {
         Button button = new Button(mContext);
-		button.setTag(DebugMenu.START_SETTLEMENT.getTag());
-		button.setText(DebugMenu.START_SETTLEMENT.getText());
-		button.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-		button.setOnClickListener(this);
+        button.setTag(DebugMenu.START_SETTLEMENT.getTag());
+        button.setText(DebugMenu.START_SETTLEMENT.getText());
+        button.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        button.setOnClickListener(this);
         return button;
     }
 
-    private Button createButtonStartField(){
+    private Button createButtonStartField() {
         Button button = new Button(mContext);
         button.setTag(DebugMenu.START_WORLD_MAP.getTag());
         button.setText(DebugMenu.START_WORLD_MAP.getText());
@@ -50,14 +50,14 @@ public class DebugActivity extends FragmentActivity implements View.OnClickListe
         return button;
     }
 
-	@Override
-	public void onClick(View view) {
-		String tag = view.getTag().toString();
-		if(DebugMenu.START_SETTLEMENT.getTag().equals(tag)){
-			startActivity(new Intent(DebugActivity.this, SettlementFieldActivity.class));
-			finish();
-		} else if(DebugMenu.START_WORLD_MAP.getTag().equals(tag)){
-			//TODO ワールドマップへ遷移
-		}
-	}
+    @Override
+    public void onClick(View view) {
+        String tag = view.getTag().toString();
+        if (DebugMenu.START_SETTLEMENT.getTag().equals(tag)) {
+            startActivity(new Intent(DebugActivity.this, SettlementFieldActivity.class));
+            finish();
+        } else if (DebugMenu.START_WORLD_MAP.getTag().equals(tag)) {
+            //TODO ワールドマップへ遷移
+        }
+    }
 }
