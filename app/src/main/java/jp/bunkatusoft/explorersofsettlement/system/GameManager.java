ba7b2c6ceb7b296c4c9fcadc462eaab810a0c0c7
s_matsumoto <s_matsumoto@axseed.co.jp>
2015-02-02 14:16:59 +0900

    [clean]使っていないメンバ変数を削除

--- app/src/main/java/jp/bunkatusoft/explorersofsettlement/system/GameManager.java
+++ app/src/main/java/jp/bunkatusoft/explorersofsettlement/system/GameManager.java
@@ -15,11 +15,9 @@ import jp.bunkatusoft.explorersofsettlement.field.world.WorldMapTask;
  */
 public class GameManager {
 
-	private Context mContext;
 	private List<Task> mTaskList = new ArrayList<Task>();
 
 	public GameManager(Context context) {
-		mContext = context;
 		//TODO ここに実行するタスクを追加
 		mTaskList.add(new WorldMapTask(context));
 	}
