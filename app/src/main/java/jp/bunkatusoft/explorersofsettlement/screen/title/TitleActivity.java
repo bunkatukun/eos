c634154a3797f1928e8c98cc2a20233443b7bb56
Munehiro Kagaya <m_kagaya@axseed.co.jp>
2015-06-11 17:15:00 +0900

    Title画面 新規開始ボタンの遷移先を WorldFieldActivity に変更

--- app/src/main/java/jp/bunkatusoft/explorersofsettlement/screen/title/TitleActivity.java
+++ app/src/main/java/jp/bunkatusoft/explorersofsettlement/screen/title/TitleActivity.java
@@ -18,9 +18,9 @@ import java.util.List;
 
 import jp.bunkatusoft.explorersofsettlement.R;
 import jp.bunkatusoft.explorersofsettlement.debug.DebugActivity;
-import jp.bunkatusoft.explorersofsettlement.field.settlement.SettlementFieldActivity;
 import jp.bunkatusoft.explorersofsettlement.screen.playdata.PlayDataActivity;
 import jp.bunkatusoft.explorersofsettlement.screen.record.RecordActivity;
+import jp.bunkatusoft.explorersofsettlement.screen.world.WorldFieldActivity;
 import jp.bunkatusoft.explorersofsettlement.system.SystemDialogView;
 import jp.bunkatusoft.explorersofsettlement.system.SystemMenuEnum;
 import jp.bunkatusoft.explorersofsettlement.system.SystemMenuView;
@@ -146,7 +146,7 @@ public class TitleActivity extends FragmentActivity implements View.OnClickListe
 		int id = view.getId();
 		switch (id) {
 			case R.id.title_part_newStartButton:
-				startActivity(new Intent(TitleActivity.this, SettlementFieldActivity.class));
+				startActivity(new Intent(TitleActivity.this, WorldFieldActivity.class));
 				finish();
 				break;
 			case R.id.title_part_continueButton:
